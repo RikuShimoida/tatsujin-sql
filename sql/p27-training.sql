a41da958db2af09bc8e7a361dfa28aa11a748687
Riku Shimoida <xiajingtianlu@gmail.com>
2026-06-08 23:20:06 +0900

    add: P27の無名ウィンドウ構文を追加

---
+++ sql/p27-training.sql
@@ -0,0 +1,4 @@
+SELECT * FROM Shohin;
+
+SELECT shohin_id, shohin_mei, hanbai_tanka,
+    AVG(hanbai_tanka) OVER (ORDER BY shohin_id ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg FROM Shohin;
