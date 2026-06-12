45184bde4cdc76d0185b878b84cf5c61e99a3a13
Riku Shimoida <xiajingtianlu@gmail.com>
2026-06-12 15:44:04 +0900

    SQLの追加

--- sql/p27-training.sql
+++ sql/p27-training.sql
@@ -2,3 +2,15 @@ SELECT * FROM Shohin;
 
 SELECT shohin_id, shohin_mei, hanbai_tanka,
     AVG(hanbai_tanka) OVER (ORDER BY shohin_id ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg FROM Shohin;
+
+SELECT shohin_id, shohin_mei, hanbai_tanka,
+    AVG(hanbai_tanka) OVER W AS moving_avg FROM Shohin
+WINDOW W AS (ORDER BY shohin_id ROWS BETWEEN 2 PRECEDING AND CURRENT ROW);
+
+SELECT shohin_id, shohin_mei, hanbai_tanka,
+    AVG(hanbai_tanka) OVER W AS moving_avg,
+    SUM(hanbai_tanka) OVER W AS moving_sum,
+    COUNT(hanbai_tanka) OVER W AS moving_count,
+    MAX(hanbai_tanka) OVER W AS moving_max
+FROM Shohin
+WINDOW W AS (ORDER BY shohin_id ROWS BETWEEN 2 PRECEDING AND CURRENT ROW);
